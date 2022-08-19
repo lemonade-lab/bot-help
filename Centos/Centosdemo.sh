a82a9ccd1939fc04d2224be58cc98748401c09fb
柠檬冲水 <916415899@qq.com>
2022-08-20 00:39:30 +0800

    Update Centosdemo.sh

--- Centos/Centosdemo.sh
+++ Centos/Centosdemo.sh
@@ -6,10 +6,10 @@ readonly Centos
 while true
 do
 OPTION=$(whiptail \
---title "Yunzai-Bot-Help" \
+--title "《Yunzai-Bot-Help》" \
 --menu "$yourv\n##上下选择##左右确定取消" \
 15 50 5 \
-"1" "使用帮助" \
+"1" "HELP设置" \
 "2" "V2管理" \
 "3" "V3管理"  \
 "4" "运行管理" \
@@ -19,10 +19,8 @@ OPTION=$(whiptail \
 "8" "环境管理" \
 "9" "系统语言" \
 "10" "SSH管理" \
-"11" "HELP设置" \
-"12" "Bot多开" \
-"13" "防火墙" \
-"14" "系统重启" \
+"11" "防火墙" \
+"12" "系统重启" \
 3>&1 1>&2 2>&3)
 
 x=$?
@@ -31,6 +29,7 @@ then
     if [ $OPTION = 1 ]
     then
     cd "${Centos}"
+    ./Update.sh
     fi
     if [ $OPTION = 2 ]
     then
@@ -80,21 +79,19 @@ then
     if [ $OPTION = 11 ]
     then
     cd "${Centos}"
-    ./Update.sh
-    fi
-    if [ $OPTION = 12 ]
-    then
-    echo "测试中..."
-    fi
-    if [ $OPTION = 13 ]
-    then
-    cd "${Centos}"
     ./Firewalldemo.sh
     fi
-    if [ $OPTION = 14 ]
+    
+    if [ $OPTION = 12 ]
     then
-    read -p "任意值返回，输入0确定：" x
-    if [ $x = 0 ]
+    Choise=$(whiptail \
+    --title "《Yunzai-Bot-Help》" \
+    --menu "$yourv\n##上下选择##左右确定取消" \
+    15 50 5 \
+    "1" "重启" \
+    3>&1 1>&2 2>&3)
+    y=$?
+    if [ $y = 0 ]
     then
     shutdown -r now
     fi
