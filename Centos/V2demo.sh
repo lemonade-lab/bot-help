420dfacf0dd312422ee7f6ea79fa5b5a5c0e9b5f
柠檬冲水 <916415899@qq.com>
2022-08-21 00:40:10 +0800

    Update V2demo.sh

--- Centos/V2demo.sh
+++ Centos/V2demo.sh
@@ -21,7 +21,7 @@ readonly Yunzai22
 while true
 do
 OPTION=$(whiptail \
---title "Yunzai-Bot-Help" \
+--title "《Yunzai-Bot-HelpV1.1.5》" \
 --menu "$yourv\n$news" \
 15 50 3 \
 "1" "安装" \
@@ -149,9 +149,9 @@ cd ${myadress}""
     if [ $OPTION = 6 ]
     then
 Choise=$(whiptail \
---title "《Yunzai-Bot-Help》" \
+--title "《Yunzai-Bot-HelpV1.1.5》" \
 --menu "$yourv\n##上下选择##左右确定取消" \
-15 50 5 \
+15 50 3 \
 "1" "卸载" \
 3>&1 1>&2 2>&3)
 y=$?
