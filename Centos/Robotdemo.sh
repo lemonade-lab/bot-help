d383371a95bff0c47ad605fec2382a9a8a44eded
柠檬冲水 <916415899@qq.com>
2022-08-20 23:02:52 +0800

    Update Robotdemo.sh

--- Centos/Robotdemo.sh
+++ Centos/Robotdemo.sh
@@ -38,12 +38,12 @@ OPTION=$(whiptail \
 x=$?
 if [ $x = 0 ]
 then
-     fi [ $OPTION = 1 ]
+     if [ $OPTION = 1 ]
      then
 cd "${myadress}"
 pm2 list
      fi
-     fi [ $OPTION = 2 ]
+     if [ $OPTION = 2 ]
      then
 [ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
 [ -e ${Yunzai22}"/config/config.js" ] || news="#您未配置机器人V2QQ,需要初始化"
@@ -52,7 +52,7 @@ pm2 list
 [ ! -e ${Yunzai22}"/config/config.js" ] || npm start
 [ ! -e ${Yunzai22}"/config/config.js" ] || echo "V2后台运行"
      fi
-     fi [ $OPTION = 3 ]
+     if [ $OPTION = 3 ]
      then
 [ -d ${Yunzai33}"/plugins" ] || news="#未安装V3！"
 [ -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#您未配置机器人V3QQ"
@@ -61,21 +61,21 @@ pm2 list
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm start
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#V3后台运行"
      fi
-     fi [ $OPTION = 4 ]
+     if [ $OPTION = 4 ]
      then
 [ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
 [ ! -d ${Yunzai22}"/plugins" ] || cd ${Yunzai22}""
 [ ! -d ${Yunzai22}"/plugins" ] || npm stop
 [ ! -d ${Yunzai22}"/plugins" ] || news="#V2关闭"
      fi
-     fi [ $OPTION = 5 ]
+     if [ $OPTION = 5 ]
      then
 [ -d ${Yunzai33}"/plugins" ] || news="#未安装V3！"
 [ ! -d ${Yunzai33}"/plugins" ] || cd ${Yunzai33}""
 [ ! -d ${Yunzai33}"/plugins" ] || npm stop
 [ ! -d ${Yunzai33}"/plugins" ] || news="#V3关闭"
      fi
-     fi [ $OPTION = 6 ]
+     if [ $OPTION = 6 ]
      then
 [ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
 [ -e ${Yunzai22}"/config/config.js" ] || news="#您未配置机器人V2QQ,需要初始化"
@@ -90,7 +90,7 @@ pm2 list
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm start
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#V3后台运行"
      fi
-     fi [ $OPTION = 7 ]
+     if [ $OPTION = 7 ]
      then
 [ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
 [ ! -d ${Yunzai22}"/plugins" ] || cd ${Yunzai22}""
