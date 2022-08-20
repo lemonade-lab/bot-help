6096f05cc071814f54107c740c8f654037766fda
柠檬冲水 <916415899@qq.com>
2022-08-20 23:35:22 +0800

    Update Pm2demo.sh

--- Centos/Pm2demo.sh
+++ Centos/Pm2demo.sh
@@ -1,125 +1,119 @@
 #!/bin/bash
+yourv=$(cat /etc/redhat-release)
+readonly yourv
+
+news=""
+
 myadress="/home/lighthouse"
 readonly myadress
-[ -d /home ] || echo "警告：不是Centos系统！退出执行！"
-[ -d /home ] || exit
+
 cd /home
-[ -d ${myadress} ] || mkdir  lighthouse
-[ -d ${myadress} ] || echo "初始化lighthouse目录失败！退出执行！"
-cd ${myadress}""
-[ -d ${myadress}"/YunzaiV2" ] || mkdir  YunzaiV2
-[ -d ${myadress}"/YunzaiV3" ] || mkdir  YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || echo "初始化Yunzai目录失败！退出执行！" 
-[ -d ${myadress}"/YunzaiV3" ] || exit
-cd ${myadress}""
-PS3="请选择: "
-while true; 
+[ -d ${myadress} ] || mkdir lighthouse
+cd "${myadress}"
+[ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
+[ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
+[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
+cd "${myadress}"
+
+
+while true
 do
-echo "________________________________________"
-echo "_______《进程管理》______________________"
-echo "_______作者：bilibili柠檬冲水UP__________"
-echo "_______提示：升级维护时不需要停止机器人___"
-echo "_______提示：升级维护时不需要停止机器人___"
-echo "_______提示：运行的机器人数目取决于CPU____"
-echo "_______2022年8月16日V1.1.4______________"
-echo "________________________________________"
-fruits1=(
- '安装PM2' 
- '查看进程列表' 
- '监视所有进程'  
- '运行所有进程' 
- '停止指定进程'
- '停止所有进程'
- '重启指定进程'
- '重启所有进程'
- '杀死特定进程'
- '杀死所有进程'
- '显示进程日记' 
- '返回'
-)
-select version1 in ${fruits1[@]}
-do
-case $version1 in
-'安装PM2')
-clear
-cd ${myadress}""
-npm i pm2 -g   
-echo "安装执行已执行！"
-break
-;;
-'查看进程列表')
-clear
-cd ${myadress}""
+OPTION=$(whiptail \
+--title "Yunzai-Bot-Help" \
+--menu "$yourv\n$news" \
+15 50 3 \
+"1" "安装PM2" \
+"2" "查看进程列表" \
+"3" "监视所有进程" \
+"4" "运行所有进程" \
+"5" "停止指定进程" \
+"6" "停止所有进程" \
+"7" "重启指定进程" \
+"8" "重启所有进程" \
+"9" "杀死特定进程" \
+"10" "杀死所有进程" \
+"11" "显示进程日记" \
+3>&1 1>&2 2>&3)
+
+x=$?
+if [ $x = 0 ]
+then
+
+    if [ $OPTION = 1 ]
+    then
+cd "${myadress}"
+npm i pm2 -g
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 2 ]
+    then
+cd "${myadress}"
 pm2 list
-break
-;;
-'监视所有进程')
-clear
-cd ${myadress}""
-pm2 monit
-break
-;;
-'运行所有进程')
-clear
-cd ${myadress}""
-pm2 start all
-break
-;;
-'停止指定进程')
-clear
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 3 ]
+    then
+cd "${myadress}"
+pm2 monit    
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 4 ]
+    then
+cd "${myadress}"
+pm2 start all    
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 5 ]
+    then
+cd "${myadress}"
 read -p "进程ID：" x
 pm2 list
 cd ${myadress}""
-pm2 stop $x
-break
-;;
-'停止所有进程')
-clear
-cd ${myadress}""
-pm2 stop all
-break
-;;
-'重启指定进程')
-clear
+pm2 stop $x    
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 6 ]
+    then
+cd "${myadress}"
+pm2 stop all    
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 7 ]
+    then
+cd "${myadress}"
 read -p "进程ID：" x
 cd ${myadress}""
 pm2 list
-pm2 restart $x
-break
-;;
-'重启所有进程')
-clear
-cd ${myadress}""
-pm2 restart all
-break
-;;
-'杀死特定进程')
-clear
+pm2 restart $x    
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 8 ]
+    then
+cd "${myadress}"
+pm2 restart all    
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 9 ]
+    then
+cd "${myadress}"
 read -p "进程ID：" x
 cd ${myadress}""
-pm2 restart $x
-break
-;;
-'杀死所有进程')
-clear
-cd ${myadress}""
-pm2 delete all
-break
-;;
-'显示进程日记')
-clear
-cd ${myadress}""
-pm2 logs
-break
-;;
-'返回')
-clear
-exit
-;;
-*)
-clear
-echo "#您的选择不存在，请重新选择！"
-break
-esac
-done
+pm2 restart $x    
+read -p "回车并继续..."
+    fi
+     if [ $OPTION = 10 ]
+    then
+cd "${myadress}"
+pm2 delete all   
+read -p "回车并继续..."
+    fi
+     if [ $OPTION = 11 ]
+    then
+cd "${myadress}"
+pm2 logs    
+read -p "回车并继续..."
+    fi
+else
+    exit
+fi
 done
