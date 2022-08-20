497528b25be4870f1e77bca8436987ef35e0f150
柠檬冲水 <916415899@qq.com>
2022-08-20 23:51:50 +0800

    Update Systemlanguagedemo.sh

--- Centos/Systemlanguagedemo.sh
+++ Centos/Systemlanguagedemo.sh
@@ -1,59 +1,56 @@
 #!/bin/bash
+yourv=$(cat /etc/redhat-release)
+readonly yourv
+
+news=""
+
 myadress="/home/lighthouse"
 readonly myadress
-[ -d /home ] || echo "警告：不是Centos系统！退出执行！"
-[ -d /home ] || exit
+
 cd /home
-[ -d ${myadress} ] || mkdir  lighthouse
-[ -d ${myadress} ] || echo "初始化lighthouse目录失败！退出执行！"
-cd ${myadress}""
-[ -d ${myadress}"/YunzaiV2" ] || mkdir  YunzaiV2
-[ -d ${myadress}"/YunzaiV3" ] || mkdir  YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || echo "初始化Yunzai目录失败！退出执行！" 
-[ -d ${myadress}"/YunzaiV3" ] || exit
-cd ${myadress}""
-PS3="请选择: "
-while true; 
+[ -d ${myadress} ] || mkdir lighthouse
+cd "${myadress}"
+[ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
+[ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
+[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
+cd "${myadress}"
+
+
+while true
 do
-echo "________________________________________"
-echo "_______《系统语言管理》"
-echo "_______作者：bilibili柠檬冲水UP"
-echo "_______系统语言修改完成后重启生效"
-echo "_______2022年8月16日V1.1.4"
-echo "______________________"
-fruits1=(
- '下载语言包' 
- '语言列表'     
- '当前语言' 
- '编辑教程' 
- '编辑语言' 
- '重启' 
- '返回'
-)
-select version1 in ${fruits1[@]}
-do
-case $version1 in
-'下载语言包')
-clear
+OPTION=$(whiptail \
+--title "Yunzai-Bot-Help" \
+--menu "$yourv\n$news" \
+15 50 3 \
+"1" "下载语言包" \
+"2" "语言列表" \
+"3" "当前语言" \
+"4" "编辑教程" \
+"5" "编辑语言" \
+3>&1 1>&2 2>&3)
+
+x=$?
+if [ $x = 0 ]
+then
+    if [ $OPTION = 1 ]
+    then
+cd "${myadress}"
 dnf install langpacks-zh_CN
 nf install -y ibus-libpinyin.x86_64
-cd ${myadress}""
-
-break
-;;
-'语言列表')
-clear
+news="#已执行！"
+    fi
+    if [ $OPTION = 2 ]
+    then
 locale -a
-cd ${myadress}""
-break
-;;
-'当前语言')
-clear
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 3 ]
+    then
 echo $LANG
-cd ${myadress}""
-break
-;;
-'编辑教程')
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 4 ]
+    then
 echo "__________________________________________________"
 echo "请查所有语言，找到zh_CN.utf8类似字眼则说明支持中文包"
 echo "记住自己的中文包名字，选择编辑时，改为中文即可"
@@ -63,29 +60,14 @@ echo "按i进入修改模式"
 echo "按ESE退出修改模式"
 echo "输入:wq!强制保存"
 echo "按CTRL+Z退出文件"
-cd ${myadress}""
-break
-;;
-'编辑语言')
+read -p "回车并继续..."
+    fi
+    if [ $OPTION = 5 ]
+    then
 vi /etc/locale.conf
-cd ${myadress}""
-break
-;;
-'重启')
-cd ${myadress}""
-echo "执行重启，请重新登录！"
-shutdown -r now
-exit
-;;
-'返回')
-cd ${myadress}""
-exit
-;;
-*)
-clear
-echo "#您的选择不存在，请重新选择！"
-cd ${myadress}""
-break
-esac
-done
+cd "${myadress}"
+    fi
+else
+    exit
+fi
 done
