8e25ef4d2d2735ced7161756516a61503b2c46d5
柠檬冲水 <916415899@qq.com>
2022-08-28 17:17:31 +0800

    Update Update.sh

--- Centos/Update.sh
+++ Centos/Update.sh
@@ -3,8 +3,6 @@
 yourv=$(cat /etc/redhat-release)
 readonly yourv
 
-news="默认强制更新..."
-
 myadress="/home/lighthouse"
 readonly myadress
 
@@ -18,14 +16,15 @@ cd /home
 cd "${myadress}"
 [ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || echo "#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || read -p "#回车并继续..." c
 cd "${myadress}"
 
 while true
 do
 OPTION=$(whiptail \
 --title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n$news" \
+--menu "$yourv" \
 15 50 3 \
 "1" "更新" \
 "2" "卸载" \
@@ -41,13 +40,12 @@ then
 [ -d ${bot} ] || cd /
 [ -d ${bot} ] || "https://github.com/ningmengchongshui"${bot}".git"
 [ -e ${Centosdemo} ] || rm -rf "${bot}"
-[ -e ${Centosdemo} ] || news="#操作失败了，请重新执行！"
+[ -e ${Centosdemo} ] || echo "#操作失败了，请重新执行！"
 [ ! -e ${Centosdemo} ] || cd "${bot}"
 [ ! -e ${Centosdemo} ] || git fetch --all
 [ ! -e ${Centosdemo} ] || git reset --hard main
 [ ! -e ${Centosdemo} ] || git pull
-[ ! -e ${Centosdemo} ] || news="#执行完成，请重新授权！"
-[ ! -e ${Centosdemo} ] || cd "${myadress}"
+[ ! -e ${Centosdemo} ] || echo "#执行完成，请重新授权！"
 read -p "回车并继续..." y
      fi
      
@@ -65,6 +63,7 @@ if [ $y = 0 ]
 then
 sudo su root
 rm -rf "${bot}"
+echo "#执行完成"
 read -p "回车并继续..." y
 fi
      fi
