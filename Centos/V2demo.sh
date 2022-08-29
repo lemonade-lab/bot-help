b91d2289b822a0baa081ca66e1fdb2caf2e1a608
柠檬冲水 <916415899@qq.com>
2022-08-29 21:35:59 +0800

    Update V2demo.sh

--- Centos/V2demo.sh
+++ Centos/V2demo.sh
@@ -3,8 +3,6 @@
 yourv=$(cat /etc/redhat-release)
 readonly yourv
 
-news="#若安装过程中卡顿可退出重新执行"
-
 myadress="/home/lighthouse"
 readonly myadress
 
@@ -13,7 +11,8 @@ cd /home
 cd "${myadress}"
 [ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || echo "#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || read -p "#回车并继续..." c
 cd "${myadress}"
 
 Yunzai22="${myadress}/YunzaiV2/Yunzai-Bot"
@@ -23,8 +22,8 @@ while true
 do
 OPTION=$(whiptail \
 --title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n$news" \
-15 50 3 \
+--menu "$yourv" \
+15 50 5 \
 "1" "安装" \
 "2" "启动" \
 "3" "修改配置" \
@@ -58,6 +57,7 @@ yum install -y nodejs
 yum install -y npm
 fi
 
+
 ##redis
 redis-server -v
 if [ $? != 0 ]
@@ -75,25 +75,30 @@ then
 yum -y install git
 fi
 
+
 ##yunzai
-[ -d ${myadress}"/YunzaiV2" ] || news="#初始化失败"
+[ -d ${myadress}"/YunzaiV2" ] || echo "#回车并继续..."
+[ -d ${myadress}"/YunzaiV2" ] || read -p "#回车并继续..." c
 [ -d ${myadress}"/YunzaiV2" ] || break
-cd ${myadress}"/YunzaiV2"
-[ -d ${Yunzai22}"/plugins" ] || git clone https://gitee.com/yoimiya-kokomi/Yunzai-Bot
+cd ${myadress}"/YunzaiV3"
+[ -d ${Yunzai22}"/plugins" ] || git clone https://gitee.com/Le-niao/Yunzai-Bot.git
 [ -d ${Yunzai22}"/plugins" ] || rm -rf ${Yunzai22} ""
-[ -d ${Yunzai22}"/plugins" ] || news="#安装失败" 
+[ -d ${Yunzai22}"/plugins" ] || echo "#安装失败" 
+[ -d ${Yunzai22}"/plugins" ] || read -p "回车并继续..." c
 [ -d ${Yunzai22}"/plugins" ] || break
 
 ##miao
 cd "${Yunzai22}"
 [ -d ${Yunzai22}"/plugins/miao-plugin/resources" ] || git clone https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
 [ -d ${Yunzai22}"/plugins/miao-plugin/resources" ] || rm -rf ${Yunzai22}"/plugins/miao-plugin"
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources" ] || news="#安装失败"
+[ -d ${Yunzai22}"/plugins/miao-plugin/resources" ] || echo "#安装失败,已清除残留"
+[ -d ${Yunzai22}"/plugins/miao-plugin/resources" ] || read -p "回车并继续..." c
 [ -d ${Yunzai22}"/plugins/miao-plugin/resources" ] || break
 
 ##依赖
 cd "${Yunzai22}"
 npm install
+npm install image-size
 
 ##安装Chromium
 yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 -y 
@@ -101,10 +106,8 @@ yum install libdrm libgbm libxshmfence -y
 yum install nss -y
 yum update nss -y
 yum groupinstall fonts -y
-
-##再此确认Chromium
 node ./node_modules/puppeteer/install.js
-news="#安装成功"
+echo "#安装成功"
 
 ##返回
 cd "${myadress}"
@@ -114,18 +117,23 @@ read -p "回车并继续..." y
 #启动
     if [ $OPTION = 2 ]
     then
-[ -d ${Yunzai22}"/plugins" ] || news="#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || echo "#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || read -p "回车并继续..." y
 [ -d ${Yunzai22}"/plugins" ] || break
 cd "${Yunzai22}"
 node app.js
+read -p "回车并继续..." y
     fi
+    
 
 #修改
     if [ $OPTION = 3 ]
     then
-[ -d ${Yunzai22}"/plugins" ] || news="#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || echo "#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || read -p "回车并继续..." y
 [ -d ${Yunzai22}"/plugins" ] || break
-[ -e ${Yunzai22}"/config/config.js" ] || news="#您未配置"
+[ -e ${Yunzai22}"/config/config.js" ] || echo "#您未配置"
+[ -e ${Yunzai22}"/config/config.js" ] || read -p "回车并继续..." y
 [ -e ${Yunzai22}"/config/config.js" ] || break
 vi "${Yunzai22}/config/config.js"
     fi
@@ -133,46 +141,55 @@ vi "${Yunzai22}/config/config.js"
 #删除
     if [ $OPTION = 4 ]
     then
-[ -d ${Yunzai22}"/plugins" ] || news="#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || echo "#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || read -p "回车并继续..." y
 [ -d ${Yunzai22}"/plugins" ] || break
-[ -e ${Yunzai22}"/config/config.js" ] || news="#您未配置"
+[ -e ${Yunzai22}"/config/config.js" ] || echo "#您未配置"
+[ -e ${Yunzai22}"/config/config.js" ] || read -p "回车并继续..." y
 [ -e ${Yunzai22}"/config/config.js" ] || break
 rm -rf "${Yunzai22}/config/config.js"
-[ -e ${Yunzai22}"/config/config.js" ] || news="#配置删除"
+[ -e ${Yunzai22}"/config/config.js" ] || echo "#配置删除"
+[ -e ${Yunzai22}"/config/config.js" ] || read -p "回车并继续..." y
 [ -e ${Yunzai22}"/config/config.js" ] || break
+read -p "回车并继续..." y
     fi
     
 #更新
     if [ $OPTION = 5 ]
     then
-[ -d ${Yunzai22}"/plugins/miao-plugin" ] || news="#请先安装"
+[ -d ${Yunzai22}"/plugins/miao-plugin" ] || echo "#请先安装"
+[ -d ${Yunzai22}"/plugins/miao-plugin" ] || read -p "回车并继续..." y
 [ -d ${Yunzai22}"/plugins/miao-plugin" ] || break
 cd "${Yunzai22}"
 git pull
-cd ${Yunzai22}"/plugins/miao-plugin"
+cd "${Yunzai22}/plugins/miao-plugin"
 git pull
-news="#更新完成"
-cd "${myadress}"
+echo "#更新完成"
+read -p "回车并继续..." y
+cd ${myadress}""
     fi
     
 #卸载
     if [ $OPTION = 6 ]
     then
+[ -d ${Yunzai22}"/plugins" ] || echo "#请先安装"
+[ -d ${Yunzai22}"/plugins" ] || read -p "回车并继续..." y
+[ -d ${Yunzai22}"/plugins" ] || break
 Choise=$(whiptail \
 --title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n##上下选择##左右确定取消" \
+--menu "$yourv" \
 15 50 3 \
 "1" "卸载" \
 3>&1 1>&2 2>&3)
 y=$?
 if [ $y = 0 ]
 then
-[ -d ${Yunzai22}"/plugins" ] || news="#请先安装"
-[ -d ${Yunzai22}"/plugins" ] || break
 rm -rf "${Yunzai22}"
-[ -e ${Yunzai22} ] || news="#卸载成功！"
+[ -e ${Yunzai22} ] || echo "#卸载成功！"
+[ -e ${Yunzai22} ] || read -p "回车并继续..." y
 [ -e ${Yunzai22} ] || break
-news="#卸载失败！"
+echo "#卸载失败！"
+read -p "回车并继续..." y
 fi
     fi
     
