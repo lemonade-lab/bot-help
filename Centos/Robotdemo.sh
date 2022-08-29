9393eeb4d149dcba97e785b2338ee20943f7ee2c
柠檬冲水 <916415899@qq.com>
2022-08-29 21:44:49 +0800

    Update Robotdemo.sh

--- Centos/Robotdemo.sh
+++ Centos/Robotdemo.sh
@@ -2,7 +2,6 @@
 yourv=$(cat /etc/redhat-release)
 readonly yourv
 
-news="#若发现占用端口可使用进程管理杀死"
 
 myadress="/home/lighthouse"
 readonly myadress
@@ -25,7 +24,7 @@ while true
 do
 OPTION=$(whiptail \
 --title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n$news" \
+--menu "$yourv" \
 15 50 3 \
 "1" "运行状态" \
 "2" "启动V2" \
@@ -47,66 +46,66 @@ read -p "回车并继续..." c
      fi
      if [ $OPTION = 2 ]
      then
-[ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
-[ -e ${Yunzai22}"/config/config.js" ] || news="#您未配置机器人V2QQ,需要初始化"
+[ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
+[ -e ${Yunzai22}"/config/config.js" ] || echo "#您未配置机器人V2QQ,需要初始化"
 [ ! -e ${Yunzai22}"/config/config.js" ] || cd ${Yunzai22}""
 [ ! -e ${Yunzai22}"/config/config.js" ] || npm stop
 [ ! -e ${Yunzai22}"/config/config.js" ] || npm start
-[ ! -e ${Yunzai22}"/config/config.js" ] || news="#V2后台运行"
+[ ! -e ${Yunzai22}"/config/config.js" ] || echo "#V2后台运行"
 read -p "回车并继续..." c
      fi
      if [ $OPTION = 3 ]
      then
-[ -d ${Yunzai33}"/plugins" ] || news="#未安装V3！"
-[ -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#您未配置机器人V3QQ"
+[ -d ${Yunzai33}"/plugins" ] || echo "#未安装V3！"
+[ -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#您未配置机器人V3QQ"
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || cd ${Yunzai33}""
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm stop
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm start
-[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#V3后台运行"
+[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#V3后台运行"
 read -p "回车并继续..." c
      fi
      if [ $OPTION = 4 ]
      then
-[ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
+[ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
 [ ! -d ${Yunzai22}"/plugins" ] || cd ${Yunzai22}""
 [ ! -d ${Yunzai22}"/plugins" ] || npm stop
-[ ! -d ${Yunzai22}"/plugins" ] || news="#V2关闭"
+[ ! -d ${Yunzai22}"/plugins" ] || echo "#V2关闭"
 read -p "回车并继续..." c
      fi
      if [ $OPTION = 5 ]
      then
-[ -d ${Yunzai33}"/plugins" ] || news="#未安装V3！"
+[ -d ${Yunzai33}"/plugins" ] || echo "#未安装V3！"
 [ ! -d ${Yunzai33}"/plugins" ] || cd ${Yunzai33}""
 [ ! -d ${Yunzai33}"/plugins" ] || npm stop
-[ ! -d ${Yunzai33}"/plugins" ] || news="#V3关闭"
+[ ! -d ${Yunzai33}"/plugins" ] || echo "#V3关闭"
 read -p "回车并继续..." c
      fi
      if [ $OPTION = 6 ]
      then
-[ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
-[ -e ${Yunzai22}"/config/config.js" ] || news="#您未配置机器人V2QQ,需要初始化"
+[ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
+[ -e ${Yunzai22}"/config/config.js" ] || echo "#您未配置机器人V2QQ,需要初始化"
 [ ! -e ${Yunzai22}"/config/config.js" ] || cd ${Yunzai22}""
 [ ! -e ${Yunzai22}"/config/config.js" ] || npm stop
 [ ! -e ${Yunzai22}"/config/config.js" ] || npm start
-[ ! -e ${Yunzai22}"/config/config.js" ] || news="#V2后台运行"
-[ -d ${Yunzai33}"/plugins" ] || news="#未安装V3！"
-[ -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#您未配置机器人V3QQ,需要初始化"
+[ ! -e ${Yunzai22}"/config/config.js" ] || echo "#V2后台运行"
+[ -d ${Yunzai33}"/plugins" ] || echo "#未安装V3！"
+[ -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#您未配置机器人V3QQ,需要初始化"
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || cd ${Yunzai33}""
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm stop
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm start
-[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || news="#V3后台运行"
+[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#V3后台运行"
 read -p "回车并继续..." c
      fi
      if [ $OPTION = 7 ]
      then
-[ -d ${Yunzai22}"/plugins" ] || news="#未安装V2！"
+[ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
 [ ! -d ${Yunzai22}"/plugins" ] || cd ${Yunzai22}""
 [ ! -d ${Yunzai22}"/plugins" ] || npm stop
-[ ! -d ${Yunzai22}"/plugins" ] || news="#V2关闭"
-[ -d ${Yunzai33}"/plugins" ] || news="#未安装V3！"
+[ ! -d ${Yunzai22}"/plugins" ] || echo "#V2关闭"
+[ -d ${Yunzai33}"/plugins" ] || echo "#未安装V3！"
 [ ! -d ${Yunzai33}"/plugins" ] || cd ${Yunzai33}""
 [ ! -d ${Yunzai33}"/plugins" ] || npm stop
-[ ! -d ${Yunzai33}"/plugins" ] || news="#V3关闭"
+[ ! -d ${Yunzai33}"/plugins" ] || echo "#V3关闭"
 read -p "回车并继续..." c
      fi
 else
