17a1bf9e3de8e8bad01c23ea10598fb45cdb8cc5
柠檬冲水 <916415899@qq.com>
2022-08-31 11:06:37 +0800

    Update Extensiondemo.sh

--- Ubuntu/Extensiondemo.sh
+++ Ubuntu/Extensiondemo.sh
@@ -2,8 +2,6 @@
 yourv=$(cat /etc/issue)
 readonly yourv
 
-news=""
-
 myadress="/home/lighthouse"
 readonly myadress
 
@@ -12,7 +10,6 @@ cd /home
 cd "${myadress}"
 [ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
 cd "${myadress}"
 Yunzai22="${myadress}/YunzaiV2/Yunzai-Bot"
 readonly Yunzai22
@@ -22,7 +19,7 @@ readonly Yunzai33
 while true
 do
 OPTION=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
+--title "《Yunzai-Bot》" \
 --menu "$yourv\n$news" \
 15 50 3 \
 "1" "派梦语音" \
@@ -36,24 +33,24 @@ if [ $x = 0 ]
 then
     if [ $OPTION = 1 ]
     then
-[ -d ${Yunzai33}"/resources" ] || echo "未安装V3！"
+[ -d ${Yunzai33}"/resources" ] || echo "#未安装V3！"
 [ -d ${Yunzai33}"/resources" ] || break
 cd ${Yunzai33}"/resources"
 [ -d ${Yunzai33}"/resources/voice" ] || mkdir voice
-[ -d ${Yunzai33}"/resources/voice" ] || echo "初始化失败"
+[ -d ${Yunzai33}"/resources/voice" ] || echo "#初始化失败"
 [ -d ${Yunzai33}"/resources/voice" ] || break
 cd ${Yunzai33}"/resources/voice"
 [ -d ${Yunzai33}"/resources/voice/paimeng" ] || mkdir paimeng
-[ -d ${Yunzai33}"/resources/voice/paimeng" ] || echo "初始化失败"
+[ -d ${Yunzai33}"/resources/voice/paimeng" ] || echo "#初始化失败"
 [ -d ${Yunzai33}"/resources/voice/paimeng" ] || break
-[ -e /Yunzai-Bot-Help/Allfile/voice/paimeng/paimeng.zip ] || echo "Yunzai-Bot-help出错"
+[ -e /Yunzai-Bot-Help/Allfile/voice/paimeng/paimeng.zip ] || echo "#Yunzai-Bot-help出错"
 [ -e /Yunzai-Bot-Help/Allfile/voice/paimeng/paimeng.zip ] || break
 cd /Yunzai-Bot-Help/Allfile/voice/paimeng
 cp paimeng.zip ""${Yunzai33}"/resources/voice/paimeng"
 cd ${Yunzai33}"/resources/voice/paimeng"
 unzip paimeng.zip
-[ -e ${Yunzai33}"/plugins/example/派蒙语音v3.js" ] || echo "未安装插件-派梦语音v3.js"
-[ -e ${Yunzai33}"/plugins/example/派蒙语音v3.js" ] || echo "错误语音的js会导致报错！"
+[ -e ${Yunzai33}"/plugins/example/派蒙语音v3.js" ] || echo "#未安装插件-派梦语音v3.js"
+[ -e ${Yunzai33}"/plugins/example/派蒙语音v3.js" ] || echo "#错误语音的js会导致报错！"
 echo "配置已执行"
 read -p "回车并继续..." y
     fi
@@ -64,35 +61,35 @@ echo "V3:3"
 read -p "请选择版本：" z
 if [ $z = 2 ]
 then
-[ -d ${Yunzai22}"/plugins" ] || echo "未安装V2！"
+[ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
 [ -d ${Yunzai22}"/plugins" ] || break
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/help" ] || echo "未安装miao"
+[ -d ${Yunzai22}"/plugins/miao-plugin/resources/help" ] || echo "#未安装miao"
 [ -d ${Yunzai22}"/plugins/miao-plugin/resources/help" ] || break
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/common/theme" ] || echo "未安装miao"
+[ -d ${Yunzai22}"/plugins/miao-plugin/resources/common/theme" ] || echo "#未安装miao"
 [ -d ${Yunzai22}"/plugins/miao-plugin/resources/common/theme" ] || break
-[ -d /Yunzai-Bot-Help/Allfile/help ] || echo "Yunzai-Bot-help出错"
+[ -d /Yunzai-Bot-Help/Allfile/help ] || echo "#Yunzai-Bot-help出错"
 [ -d /Yunzai-Bot-Help/Allfile/help ] || break
 cd /Yunzai-Bot-Help/Allfile/help/V2help
 cp help-list.js ${Yunzai22}"/plugins/miao-plugin/resources/help"
 cp icon.png ${Yunzai22}"/plugins/miao-plugin/resources/help"
 cp main-01.png ${Yunzai22}"/plugins/miao-plugin/resources/common/theme"
-echo "已执行替换"
+echo "#已执行替换"
 fi
 if [ $z = 3 ]
 then
-[ -d ${Yunzai33}"/plugins" ] || echo "未安装V3！"
+[ -d ${Yunzai33}"/plugins" ] || echo "#未安装V3！"
 [ -d ${Yunzai33}"/plugins" ] || break
-[ -d ${Yunzai33}"/plugins/miao-plugin/resources/help" ] || echo "未安装miao"
+[ -d ${Yunzai33}"/plugins/miao-plugin/resources/help" ] || echo "#未安装miao"
 [ -d ${Yunzai33}"/plugins/miao-plugin/resources/help" ] || break
-[ -d ${Yunzai33}"/plugins/miao-plugin/resources/common/theme" ] || echo "未安装miao"
+[ -d ${Yunzai33}"/plugins/miao-plugin/resources/common/theme" ] || echo "#未安装miao"
 [ -d ${Yunzai33}"/plugins/miao-plugin/resources/common/theme" ] || break
-[ -d "/Yunzai-Bot-Help/Allfile/help" ] || echo "Yunzai-Bot-help出错"
+[ -d "/Yunzai-Bot-Help/Allfile/help" ] || echo "#Yunzai-Bot-help出错"
 [ -d "/Yunzai-Bot-Help/Allfile/help" ] || break
 cd /Yunzai-Bot-Help/Allfile/help/V3help
 cp help-list.js ${Yunzai33}"/plugins/miao-plugin/resources/help"
 cp icon.png ${Yunzai33}"/plugins/miao-plugin/resources/help"
 cp main-01.png ${Yunzai33}"/plugins/miao-plugin/resources/common/theme"
-echo "已执行替换"
+echo "#已执行替换"
 fi
 read -p "回车并继续..." y
     fi
