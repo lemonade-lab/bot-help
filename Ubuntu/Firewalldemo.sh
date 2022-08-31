bd9620a8c947d85f7df9c3159a30f9c6217e283d
柠檬冲水 <916415899@qq.com>
2022-08-31 10:37:02 +0800

    Update Firewalldemo.sh

--- Ubuntu/Firewalldemo.sh
+++ Ubuntu/Firewalldemo.sh
@@ -2,8 +2,6 @@
 yourv=$(cat /etc/issue)
 readonly yourv
 
-news=""
-
 myadress="/home/lighthouse"
 readonly myadress
 
@@ -12,15 +10,16 @@ cd /home
 cd "${myadress}"
 [ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || echo "初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || read -p "回车并继续..." c
+[ -d ${myadress}"/YunzaiV3" ] || exit
 cd "${myadress}"
-
 
 while true
 do
 OPTION=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n$news" \
+--title "《Help-Firewal》" \
+--menu "$yourv" \
 15 50 3 \
 "1" "状态" \
 "2" "端口" \
@@ -32,22 +31,18 @@ if [ $x = 0 ]
 then
     if [ $OPTION = 1 ]
     then
-firewall-cmd --state
-read -p "回车并继续..." y
+echo "待实现"
+read -p "回车并继续..." c
     fi
     if [ $OPTION = 2 ]
     then
-systemctl start firewalld.service
-read -p "开启端口：" x
-firewall-cmd --zone=public --add-port=$x/tcp --permanent
-systemctl restart firewalld.service
-firewall-cmd --reload
-news="#已执行！"
+echo "待实现"
+read -p "回车并继续..." c
     fi
     if [ $OPTION = 3 ]
     then
-systemctl restart firewalld.service
-read -p "回车并继续..." y
+echo "待实现"
+read -p "回车并继续..." c
     fi
 else
     exit
