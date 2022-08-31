9af4e84d6e0ce09ec16beb6f89ebe03f231a8876
柠檬冲水 <916415899@qq.com>
2022-08-31 10:39:11 +0800

    Update Update.sh

--- Ubuntu/Update.sh
+++ Ubuntu/Update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 yourv=$(cat /etc/issue)
 readonly yourv
-news=""
 
 myadress="/home/lighthouse"
 readonly myadress
@@ -16,13 +15,15 @@ cd /home
 cd "${myadress}"
 [ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
-[ -d ${myadress}"/YunzaiV3" ] || news="#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || echo "#初始化失败"
+[ -d ${myadress}"/YunzaiV3" ] || read -p "回车并继续..." c
+[ -d ${myadress}"/YunzaiV3" ] || exit
 cd "${myadress}"
 
 while true
 do
 OPTION=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
+--title "《Help-Update》" \
 --menu "$yourv\n$news" \
 15 50 3 \
 "1" "更新" \
@@ -39,22 +40,21 @@ then
 [ -d ${bot} ] || cd /
 [ -d ${bot} ] || "https://github.com/ningmengchongshui"${bot}".git"
 [ -e ${Centosdemo} ] || rm -rf "${bot}"
-[ -e ${Centosdemo} ] || news="#操作失败了，请重新执行！"
+[ -e ${Centosdemo} ] || echo "#操作失败了，请重新执行！"
 [ ! -e ${Centosdemo} ] || cd "${bot}"
 [ ! -e ${Centosdemo} ] || git fetch --all
 [ ! -e ${Centosdemo} ] || git reset --hard main
 [ ! -e ${Centosdemo} ] || git pull
-[ ! -e ${Centosdemo} ] || news="#执行完成，请重新授权！"
+[ ! -e ${Centosdemo} ] || echo "##执行完成，请重新授权！"
 [ ! -e ${Centosdemo} ] || cd "${myadress}"
-read -p "回车并继续..." y
      fi
      
 #卸载
      if [ $OPTION = 2 ]
      then
 Choise=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n##上下选择##左右确定取消" \
+--title "《Help-Delete》" \
+--menu "$yourv" \
 15 50 3 \
 "1" "卸载" \
 3>&1 1>&2 2>&3)
@@ -62,9 +62,10 @@ y=$?
 if [ $y = 0 ]
 then
 sudo su root
-rm -rf ${bot}""
+rm -rf "${bot}"
 fi
      fi
+     read -p "回车并继续..." y
 else
      exit
 fi
