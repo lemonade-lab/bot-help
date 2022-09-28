4ce51ab976b11859a96a0129072e966a958e305d
柠檬冲水 <916415899@qq.com>
2022-09-28 11:50:25 +0800

    Update Robotdemo.sh

--- Centos/Robotdemo.sh
+++ Centos/Robotdemo.sh
@@ -8,16 +8,12 @@ readonly myadress
 cd /home
 [ -d ${myadress} ] || mkdir lighthouse
 cd "${myadress}"
-[ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
 [ -d ${myadress}"/YunzaiV3" ] || echo "#初始化失败"
 [ -d ${myadress}"/YunzaiV3" ] || read -p "回车并继续..."
 [ -d ${myadress}"/YunzaiV3" ] || exit
 cd "${myadress}"
 
-funv2(){
-   [ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
-}
 
 Yunzai33="${myadress}/YunzaiV3/Yunzai-Bot"
 readonly Yunzai33
@@ -33,12 +29,8 @@ OPTION=$(whiptail \
 --menu "$yourv" \
 15 50 5 \
 "1" "运行状态" \
-"2" "启动V2" \
-"3" "启动V3" \
-"4" "停止V2" \
-"5" "停止V3" \
-"6" "启动V2V3" \
-"7" "停止V2V3" \
+"2" "启动V3" \
+"3" "停止V3" \
 3>&1 1>&2 2>&3)
 
 x=$?
@@ -52,16 +44,6 @@ read -p "回车并继续..." c
      fi
      if [ $OPTION = 2 ]
      then
-     funv2
-[ -e ${Yunzai22}"/config/config.js" ] || echo "#您未配置机器人V2QQ,需要初始化"
-[ ! -e ${Yunzai22}"/config/config.js" ] || cd ${Yunzai22}""
-[ ! -e ${Yunzai22}"/config/config.js" ] || npm stop
-[ ! -e ${Yunzai22}"/config/config.js" ] || npm start
-[ ! -e ${Yunzai22}"/config/config.js" ] || echo "#V2后台运行"
-read -p "回车并继续..." c
-     fi
-     if [ $OPTION = 3 ]
-     then
      funv3
 [ -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#您未配置机器人V3QQ"
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || cd ${Yunzai33}""
@@ -70,44 +52,8 @@ read -p "回车并继续..." c
 [ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#V3后台运行"
 read -p "回车并继续..." c
      fi
-     if [ $OPTION = 4 ]
+     if [ $OPTION = 3 ]
      then
-     funv2
-[ ! -d ${Yunzai22}"/plugins" ] || cd ${Yunzai22}""
-[ ! -d ${Yunzai22}"/plugins" ] || npm stop
-[ ! -d ${Yunzai22}"/plugins" ] || echo "#V2关闭"
-read -p "回车并继续..." c
-     fi
-     if [ $OPTION = 5 ]
-     then
-     funv3
-[ ! -d ${Yunzai33}"/plugins" ] || cd ${Yunzai33}""
-[ ! -d ${Yunzai33}"/plugins" ] || npm stop
-[ ! -d ${Yunzai33}"/plugins" ] || echo "#V3关闭"
-read -p "回车并继续..." c
-     fi
-     if [ $OPTION = 6 ]
-     then
-     funv2
-[ -e ${Yunzai22}"/config/config.js" ] || echo "#您未配置机器人V2QQ,需要初始化"
-[ ! -e ${Yunzai22}"/config/config.js" ] || cd ${Yunzai22}""
-[ ! -e ${Yunzai22}"/config/config.js" ] || npm stop
-[ ! -e ${Yunzai22}"/config/config.js" ] || npm start
-[ ! -e ${Yunzai22}"/config/config.js" ] || echo "#V2后台运行"
-     funv2
-[ -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#您未配置机器人V3QQ,需要初始化"
-[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || cd ${Yunzai33}""
-[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm stop
-[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || npm start
-[ ! -e ${Yunzai33}"/config/config/qq.yaml" ] || echo "#V3后台运行"
-read -p "回车并继续..." c
-     fi
-     if [ $OPTION = 7 ]
-     then
-     funv2
-[ ! -d ${Yunzai22}"/plugins" ] || cd ${Yunzai22}""
-[ ! -d ${Yunzai22}"/plugins" ] || npm stop
-[ ! -d ${Yunzai22}"/plugins" ] || echo "#V2关闭"
      funv3
 [ ! -d ${Yunzai33}"/plugins" ] || cd ${Yunzai33}""
 [ ! -d ${Yunzai33}"/plugins" ] || npm stop
