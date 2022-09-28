f76c23652da848acff9ea26fd91e8d8af3b6953e
柠檬冲水 <916415899@qq.com>
2022-09-28 11:47:50 +0800

    Update Extensiondemo.sh

--- Centos/Extensiondemo.sh
+++ Centos/Extensiondemo.sh
@@ -8,20 +8,12 @@ readonly myadress
 cd /home
 [ -d ${myadress} ] || mkdir lighthouse
 cd "${myadress}"
-[ -d ${myadress}"/YunzaiV2" ] || mkdir YunzaiV2
 [ -d ${myadress}"/YunzaiV3" ] || mkdir YunzaiV3
 [ -d ${myadress}"/YunzaiV3" ] || echo "#初始化失败"
 [ -d ${myadress}"/YunzaiV3" ] || read -p "回车并继续..." c
 cd "${myadress}"
 
-Yunzai22="${myadress}/YunzaiV2/Yunzai-Bot"
-readonly Yunzai22
 
-funv2(){
-   [ -d ${Yunzai22}"/plugins" ] || echo "#未安装V2！"
-   [ -d ${Yunzai22}"/plugins" ] || read -p "回车并继续..." x
-   [ -d ${Yunzai22}"/plugins" ] || break
-}
 
 Yunzai33="${myadress}/YunzaiV3/Yunzai-Bot"
 readonly Yunzai33
@@ -40,17 +32,16 @@ OPTION=$(whiptail \
 --title "《Help-Extension》" \
 --menu "$yourv" \
 15 50 5 \
-"1" "派梦语音" \
+"1" "逆天改命" \
 "2" "miaoHelp" \
 "3" "插件列表" \
-"4" "删除插件" \
-"5" "逆天改命" \
+"4" "派梦语音" \
 3>&1 1>&2 2>&3)
 
 x=$?
 if [ $x = 0 ]
 then
-    if [ $OPTION = 1 ]
+    if [ $OPTION = 4 ]
     then
     funv3
 cd ${Yunzai33}"/resources"
@@ -79,37 +70,7 @@ read -p "回车并继续..." y
     
     if [ $OPTION = 2 ]
     then
-Choise=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv" \
-15 50 3 \
-"1" "替换V2" \
-"2" "替换V3" \
-3>&1 1>&2 2>&3)
-y=$?
-if [ $y = 0 ]
-then
-    if [ $Choise = 1 ]
-    then
-    funv2
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/help" ] || echo "#未安装miao"
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/help" ] || read -p "回车并继续..." x
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/help" ] || break
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/common/theme" ] || echo "#未安装miao"
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/common/theme" ] || read -p "回车并继续..." x
-[ -d ${Yunzai22}"/plugins/miao-plugin/resources/common/theme" ] || break
-[ -d /Yunzai-Bot-Help/Allfile/help ] || echo "#Yunzai-Bot-help出错"
-[ -d /Yunzai-Bot-Help/Allfile/help ] || read -p "回车并继续..." x
-[ -d /Yunzai-Bot-Help/Allfile/help ] || break
-cd /Yunzai-Bot-Help/Allfile/help/V2help
-cp help-list.js ${Yunzai22}"/plugins/miao-plugin/resources/help"
-cp icon.png ${Yunzai22}"/plugins/miao-plugin/resources/help"
-cp main-01.png ${Yunzai22}"/plugins/miao-plugin/resources/common/theme"
-echo "#已执行替换"
-    fi
-    if [ $Choise = 2 ]
-    then
-    funv3
+funv3
 [ -d ${Yunzai33}"/plugins/miao-plugin/resources/help" ] || echo "#未安装miao"
 [ -d ${Yunzai33}"/plugins/miao-plugin/resources/help" ] || read -p "回车并继续..." x
 [ -d ${Yunzai33}"/plugins/miao-plugin/resources/help" ] || break
@@ -123,86 +84,20 @@ cd /Yunzai-Bot-Help/Allfile/help/V3help
 cp help-list.js ${Yunzai33}"/plugins/miao-plugin/resources/help"
 cp icon.png ${Yunzai33}"/plugins/miao-plugin/resources/help"
 cp main-01.png ${Yunzai33}"/plugins/miao-plugin/resources/common/theme"
-echo "#已执行替换"    
-    fi
+echo "#已执行替换"
 read -p "回车并继续..." y
-fi
     fi
     
     if [ $OPTION = 3 ]
     then
-Choise=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n##上下选择##左右确定取消" \
-15 50 3 \
-"1" "查看V2插件" \
-"2" "查看V3插件" \
-3>&1 1>&2 2>&3)
-y=$?
-if [ $y = 0 ]
-then
-    if [ $Choise = 1 ]
-    then
-    funv2
-[ ! -d ${Yunzai22}"/plugins" ] || cd "${Yunzai22}/lib/example"
-[ ! -d ${Yunzai22}"/plugins" ] || ls
-read -p "回车并继续..." y
-    fi
-    if [ $Choise = 2 ]
-    then
-    funv2
+    funv3
 [ ! -d ${Yunzai33}"/plugins" ] || cd "${Yunzai33}/plugins/example"
 [ ! -d ${Yunzai33}"/plugins" ] || ls    
 read -p "回车并继续..." y
     fi
-fi
-    fi
     
-    
-    if [ $OPTION = 4 ]
-    then
-Choise=$(whiptail \
---title "《Yunzai-Bot-HelpV1.1.5》" \
---menu "$yourv\n##上下选择##左右确定取消" \
-15 50 3 \
-"1" "删除V2插件" \
-"2" "删除V3插件" \
-3>&1 1>&2 2>&3)
-y=$?
-if [ $y = 0 ]
-then
-    if [ $Choise = 1 ]
-    then
-    funv2
-[ ! -d ${Yunzai22}"/plugins" ] || cd "${Yunzai22}/lib/example"
-[ ! -d ${Yunzai22}"/plugins" ] || ls
-[ ! -d ${Yunzai22}"/plugins" ] || read -p "请输入插件名：" y
-[ ! -e ${Yunzai22}"/lib/example/${y}.js" ] ||  echo "#未找到插件"
-[ ! -e ${Yunzai22}"/lib/example/${y}.js" ] ||  read -p "回车并继续..." x
-[ ! -e ${Yunzai22}"/lib/example/${y}.js" ] ||  break
-[ -e ${Yunzai22}"/lib/example/${y}.js" ] || rm -rf "${y}.js"
-[ -e ${Yunzai22}"/lib/example/${y}.js" ] || echo "#已执行"
-[ -e ${Yunzai22}"/lib/example/${y}.js" ] || read -p "回车并继续..." x
-    fi
-    if [ $Choise = 2 ]
-    then
-    funv3
-[ ! -d ${Yunzai33}"/plugins" ] || cd "${Yunzai33}/plugins/example"
-[ ! -d ${Yunzai33}"/plugins" ] || ls
-[ ! -d ${Yunzai33}"/plugins" ] || read -p "请输入插件名：" y
-[ ! -e ${Yunzai33}"/plugins/example/${y}.js" ] ||  echo "#未找到插件"
-[ ! -e ${Yunzai33}"/plugins/example/${y}.js" ] ||  read -p "回车并继续..." x
-[ ! -e ${Yunzai33}"/plugins/example/${y}.js" ] ||  break
-[ -e ${Yunzai33}"/plugins/example/${y}.js" ] || rm -rf "${y}.js"
-[ -e ${Yunzai33}"/plugins/example/${y}.js" ] || echo "#已执行"
-[ -e ${Yunzai33}"/plugins/example/${y}.js" ] || read -p "回车并继续..." x
-    fi
-fi
-    fi
-    
-        
 #逆天改命
-    if [ $OPTION = 5 ]
+    if [ $OPTION = 1 ]
     then
     funv3
     Choise=$(whiptail \
