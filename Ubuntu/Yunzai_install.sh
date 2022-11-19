a2ef0dc612cf5584373f3391a7965d306a92bb32
WinterChocolates <1728841959@qq.com>
2022-11-19 06:43:50 +0000

    新增云崽安装界面

---
+++ Ubuntu/Yunzai_install.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+yourv=$(cat /etc/issue)
+readonly yourv
+
+myadress="/home/lighthouse"
+readonly myadress
+
+export YunzaiV3="${myadress}/YunzaiV3/Yunzai-Bot"
+
+funv3(){
+	if ! [ -d "${YunzaiV3}/plugins" ];then
+		echo "未安装云崽V3"
+		read -p "回车并退出..." x
+	fi
+	break
+}
+funqq(){
+	if ! [ -e "${YunzaiV3}/config/config/qq.yaml" ];then
+		echo "请先启动云崽在配置qwq"
+	       read -p "回车并退出..." x
+	fi
+	break
+}
+while true
+do
+	OPTION=$(whiptail \
+		--title "《Yunzai-Bot-V3》" \
+		--menu "$yourv" \
+		15 50 3 \
+		"1" "安装" \
+		"2" "启动" \
+		"3" "修改配置" \
+		"4" "修改主人" \
+		"5" "删除QQ" \
+		"6" "更新" \
+		"7" "卸载" \
+		3>&1 1>&2 2>&3)
+
+	x=$?
+	if [ $x = 0 ]
+	then
+		if [ $OPTION = 1 ];then
+			if [ -d "${YunzaiV3}/plugins" ];then
+				echo "已经安装了云崽了呢..."
+				read -p "回车并退出..."
+				break
+			else
+				# 调用云崽安装脚本
+				./Install.sh
+				# 喵喵插件的安装
+				cd "${YunzaiV3}"
+				if ! [ -d "${YunzaiV3}/plugins/miao-plugin" ];then
+					echo -e "\n正在下载喵喵插件..."
+					git clone https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
+				fi
+				echo -e "喵喵插件已安装\n"
+				# 云崽依赖的安装
+				npm config set registry=https://registry.npmmirror.com/
+				echo -e "\n正在安装依赖..."
+				echo "enka-network-api依赖比较慢，请耐心等待qwq"
+				npm install pnpm -g
+				pnpm install -P
+				pnpm add image-size -w
+				read -p "回车并继续..." c
+			fi
+		fi
+		if [ $OPTION = 2 ];then
+			funv3
+			echo -e "\n可以在运行云崽的时候Ctrl+C退出运行"
+			echo "回到管理界面"
+			echo -e "\033[31m！！注意：使用Ctrl+C之后不会云崽将不会运行！！\033[0m\n"
+			redis-server --daemonize yes
+			cd "${YunzaiV3}"
+			node app.js
+		fi
+		if [ $OPTION = 3 ];then
+			funv3
+			funqq
+			vi ${YunzaiV3}"/config/config/group.yaml"
+			read -p "回车并退出..." c
+
+		fi
+		if [ $OPTION = 4 ];then
+			funv3
+			funqq
+			vi ${YunzaiV3}"/config/config/other.yaml"
+			read -p "回车并退出..." c
+		fi
+		if [ $OPTION = 5 ];then
+			funv3
+			funqq
+			rm -rf ${YunzaiV3}"/config/config/qq.yaml"
+			echo "删除成功！"
+			read -p "回车并退出..." c
+		fi
+		if [ $OPTION = 6 ];then
+			funv3
+			echo "私聊机器人发送#全部更新"
+			read -p "回车并退出..." c
+
+		fi
+		if [ $OPTION = 7 ];then
+			funv3
+			Choise=$(whiptail \
+				--title "《Yunzai-Bot-Delete》" \
+				--menu "$yourv" \
+				15 50 3 \
+				"1" "卸载" \
+				3>&1 1>&2 2>&3)
+
+			y=$?
+			if [ $y = 0 ]
+			then
+				rm -rf "${Yunzai33}"
+				echo "#卸载成功！"
+				read -p "回车并继续..." c
+			fi
+		fi
+	else
+		exit
+	fi
+done
