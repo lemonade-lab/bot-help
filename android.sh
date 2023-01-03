40de759b67f57e71fb26ddc45edde2406fb55e6a
916415899@qq.com <916415899@qq.com>
2023-01-03 18:16:21 +0800

    1.4

---
+++ android.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+apt install wget proot -y
+wget https://github.com/xiliuya/termux-linux/releases/download/untagged-d42e22a770f3c95aac45/install_linux.sh ~/install_linux.sh
+chmod +x ~/install_linux.sh
+~/install_linux.sh
+~/start-ubuntu.sh
