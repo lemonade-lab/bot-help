dafee5db7556b834af09976082014e65e67e2f96
柠檬冲水 <916415899@qq.com>
2023-01-07 00:53:10 +0800

    维护重写环境管理中....

--- centos/environ.sh
+++ centos/environ.sh
@@ -13,17 +13,16 @@ OPTION=$(whiptail \
 --menu "$version" \
 15 50 5 \
 "1" "环境search" \
-"2" "安装Redis" \
-"3" "安装zip" \
-"4" "安装sshpass" \
-"5" "安装Python" \
-"6" "安装ffmpeg" \
+"2" "更新updata" \
+"3" "安装install" \
+"4" "卸载uninstall" \
 3>&1 1>&2 2>&3)
 feedback=$?
 
 if [ $feedback = 0 ]
 then
 
+#环境
     if [ $OPTION = 1 ]
     then 
     echo "nodejs"
@@ -34,29 +33,28 @@ then
     git version
     read -p "回车并继续Enter..." y
     fi
-
+   
+#更新
     if [ $OPTION = 2 ]
-    then ps -aux | grep redis
+    then yum update -y
     read -p "回车并继续Enter..." y
     fi
-
+    
+#安装
     if [ $OPTION = 3 ]
-    then yum install -y unzip zip
-    fi
-
-    if [ $OPTION = 4 ]
-    then
+    then        
+    #安装zip
+    yum install -y unzip zip
+    read -p "回车并sshpass继续Enter..." y
+    #安装sshpass
     cd "${myadress}"
     wget https://nchc.dl.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar
     tar  zxf sshpass-1.06.tar.gz
     ./configure  --prefix=/usr/local/
     make && make install
     rm -rf sshpass-1.06.tar.gz
-    read -p "回车并继续Enter..." y
-    fi
-
-    if [ $OPTION = 5 ]
-    then
+    read -p "回车并py继续Enter..." y
+    #安装py
     wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tgz
     tar -zxvf Python-3.9.0.tgz
     rm -rf Python-3.9.0.tgz
@@ -65,11 +63,8 @@ then
     make && make install
     ln -s /usr/local/python3/bin/python3.9 /usr/bin/python3
     ln -s /usr/local/python3/bin/pip3.9 /usr/bin/pip3
-    read -p "回车并继续Enter..." y
-    fi
-
-    if [ $OPTION = 6 ]
-    then
+    read -p "回车并ffmpeg继续Enter..." y
+    #安装ffmpeg
     dnf install epel-release
     yum config-manager -set-enabled PowerTools
     yum -y install yum-utils
@@ -79,6 +74,12 @@ then
     read -p "回车并继续Enter..." y
     fi
 
+#卸载
+    if [ $OPTION = 4 ]
+    then 
+    read -p "回车并继续Enter..." y
+    fi
+    
 else
     exit
 fi
