78c8b2d9f46482a940bfe0b8d945859e2cbc0ff1
柠檬冲水 <916415899@qq.com>
2023-01-07 12:03:34 +0800

    运行管理新增安装

--- centos/peration.sh
+++ centos/peration.sh
@@ -30,44 +30,52 @@ do
         --title "《Yunzai-Bot》" \
         --menu "$version" \
         15 50 5 \
-        "1" "运行状态status" \
-		"2" "启动云崽run" \
-		"3" "停止云崽stop" \
+        "1" "安装installPM2" \
+        "2" "运行状态status" \
+	"3" "启动云崽run" \
+	"4" "停止云崽stop" \
         3>&1 1>&2 2>&3)
         feedback=$?
 	    if [ $feedback = 0 ]
 	    then
+	    
+	        if [ $OPTION = 1 ]
+		then 
+			cd "${myadress}"
+			npm install pm2 -g
+			read -p "Enter回车并继续..." c
+	     	fi
 		
-		    if [ $OPTION = 1 ]
-		    then
+		if [ $OPTION = 2 ]
+		then
 			cd "${myadress}"
 			pm2 list
 			read -p "Enter回车并继续..." c
 	     	fi
 
-		    if [ $OPTION = 2 ]
-		    then yunzaiverification
-			    if [ $? = "0" ]
-				then 
+		if [ $OPTION = 3 ]
+		then yunzaiverification
+			if [ $? = "0" ]
+			then 
 	    		[ -e "${yunzaiqq}" ] || echo "#您未配置机器人V3QQ"
 	      		[ ! -e "${yunzaiqq}" ] || cd "${yunzai}"
 	    		[ ! -e "${yunzaiqq}" ] || npm stop
 	    		[ ! -e "${yunzaiqq}" ] || npm start
 	    		[ ! -e "${yunzaiqq}" ] || echo "后台运行"
 	     		read -p "Enter回车并继续..." c
-				fi
+			fi
 	     	fi
 
-	    	if [ $OPTION = 3 ]
+	    	if [ $OPTION = 4 ]
 	    	then yunzaiverification
-			    if [ $? = "0" ]
-				then 
+			if [ $? = "0" ]
+			then 
 		     	[ ! -d "${yunzaiplugin}" ] || cd "${yunzai}"
 		    	[ ! -d "${yunzaiplugin}" ] || npm stop
 		    	[ ! -d "${yunzaiplugin}" ] || echo "关闭"
 	    		read -p "Enter回车并继续..." c
-		 		fi
-		    fi
+		 	fi
+		fi
     else
         exit
     fi
