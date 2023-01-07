6e8be342be3daa8775f3ee013eecbed368013ce5
柠檬冲水 <916415899@qq.com>
2023-01-07 11:45:01 +0800

    增加防火墙管理

--- centos/firewall.sh
+++ centos/firewall.sh
@@ -8,29 +8,48 @@ do
 OPTION=$(whiptail \
 --title "《Firewall》" \
 --menu "$version" \
-15 50 3 \
-"1" "状态" \
-"2" "端口" \
-"3" "重启" \
+15 50 5 \
+"1" "安装" \
+"2" "状态" \
+"3" "列表" \
+"4" "端口" \
+"5" "重启" \
+"6" "开启" \
+"7" "关闭" \
 3>&1 1>&2 2>&3)
 
 feedback=$?
 if [ $feedback = 0 ]
 then
     if [ $OPTION = 1 ]
-    then firewall-cmd --state
+    then yum install firewall
     read -p "已执行,回车并继续Enter..." c
     fi
     if [ $OPTION = 2 ]
-    then systemctl start firewalld.service
-    read -p "开启端口:" x
-    firewall-cmd --zone=public --add-port=$x/tcp --permanent
-    systemctl restart firewalld.service
-    firewall-cmd --reload
+    then firewall-cmd --state
     read -p "已执行,回车并继续Enter..." c
     fi
     if [ $OPTION = 3 ]
-    then systemctl restart firewalld.service
+    then firewall-cmd --zone=public --list-ports
+    read -p "已执行,回车并继续Enter..." c
+    fi
+    if [ $OPTION = 4 ]
+    then systemctl start firewalld.service
+    read -p "开启端口:" port
+    firewall-cmd --zone=public --add-port=$port/tcp --permanent
+    systemctl restart firewalld
+    read -p "已执行已重启,回车并继续Enter..." c
+    fi
+    if [ $OPTION = 5 ]
+    then systemctl restart firewalld
+    read -p "已执行,回车并继续Enter..." c
+    fi
+    if [ $OPTION = 6 ]
+    then systemctl start firewalld
+    read -p "已执行,回车并继续Enter..." c
+    fi
+    if [ $OPTION = 7 ]
+    then systemctl stop firewalld
     read -p "已执行,回车并继续Enter..." c
     fi
 else
