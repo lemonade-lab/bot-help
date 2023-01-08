412c7c7a51962a7da05067c2c7547e310d5e4e62
WinterChocolates <1728841959@qq.com>
2023-01-08 11:14:25 +0000

    1.4

--- centos/index.sh
+++ centos/index.sh
@@ -31,52 +31,52 @@ then
 
 #工具管理HelpManage
     if [ $OPTION = 1 ]
-    then sh "${centos}/shellupdata.sh"
+    then bash "${centos}/shellupdata.sh"
     fi
 
 #机器管理administrat
     if [ $OPTION = 2 ]
-    then sh "${centos}/instart.sh"
+    then bash "${centos}/instart.sh"
     fi
 
 #运行管理BackControl
     if [ $OPTION = 3 ]
-    then sh "${centos}/peration.sh"
+    then bash "${centos}/peration.sh"
     fi
     
 #进程管理processManage
     if [ $OPTION = 4 ]
-    then sh "${centos}/process.sh"
+    then bash "${centos}/process.sh"
     fi
   
 #负载管理loadManage
     if [ $OPTION = 5 ]
-    then sh "${centos}/load.sh"
+    then bash "${centos}/load.sh"
     fi
 
 #桌面安装DesktopInstall
     if [ $OPTION = 6 ]
-    then sh "${centos}/desktop.sh"
+    then bash "${centos}/desktop.sh"
     fi
 
 #环境管理EnvironManage
     if [ $OPTION = 7 ]
-    then sh "${centos}/environ.sh"
+    then bash "${centos}/environ.sh"
     fi
 
 #系统语言SystemLanguage
     if [ $OPTION = 8 ]
-    then sh "${centos}/systemlanguage.sh"
+    then bash "${centos}/systemlanguage.sh"
     fi
 
 #远程管理SSHManage
     if [ $OPTION = 9 ]
-    then sh "${centos}/longrange.sh"
+    then bash "${centos}/longrange.sh"
     fi
 
 #防火设置firewall
     if [ $OPTION = 10 ]
-    then sh "${centos}/firewall.sh"
+    then bash "${centos}/firewall.sh"
     fi
 
 #系统重启restart
