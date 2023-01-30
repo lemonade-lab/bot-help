b0ba813076e7e8a5e83f2b305d6d9fe8fed727ca
WinterChocolates <1728841959@qq.com>
2023-01-30 23:09:18 +0800

    删除android.sh

--- android.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/
-apt update
-apt upgrade -y
-apt install wget proot -y
-wget https://github.com/xiliuya/termux-linux/releases/download/untagged-d42e22a770f3c95aac45/install_linux.sh ~/install_linux.sh
-chmod +x ~/install_linux.sh
-~/install_linux.sh
