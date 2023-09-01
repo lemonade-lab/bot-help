9d0f83e4c20a8cf90a2f8d89f805f8fad7a05d60
柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>
2023-09-01 07:06:19 +0000

    update install.sh.
    Signed-off-by: 柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>

--- install.sh
+++ install.sh
@@ -19,6 +19,24 @@ elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"
 then release="centos"
 fi
 
+
+# 检查目录是否存在，不存在则创建
+if [ ! -d "/usr/local/bin" ]; then
+    sudo mkdir -p /usr/local/bin
+fi
+
+# 检查脚本文件是否已经存在内容
+if [ ! -s "/usr/local/bin/bot-help" ]; then
+    # 写入脚本内容到文件
+    sudo tee /usr/local/bin/bot-help >/dev/null <<EOF
+#!/bin/bash
+/bot-help/install.sh
+EOF
+
+    # 设置脚本文件的可执行权限
+    sudo chmod +x /usr/local/bin/bot-help
+fi
+
 if [ ${release} = "centose" ]
 then bash "${appname}/centos/index.sh"
 fi
